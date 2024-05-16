d62cfb5b480a43870991b08d494b334e49ac9e33
Michal Boguszewski <michal1842001@gmail.com>
2024-05-16 12:29:04 +0200

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+use glium::Surface;
+use winit::{event, event_loop};
+use winit::event::WindowEvent;
+
+fn main() {
+    let mut width = 800;
+    let mut height = 600;
+
+    let event_loop = winit::event_loop::EventLoopBuilder::new().build();
+    let (window, display) = glium::backend::glutin::SimpleWindowBuilder::new()
+        .with_title("Duck")
+        .with_inner_size(width, height)
+        .build(&event_loop);
+
+    let mut egui_glium = egui_glium::EguiGlium::new(&display, &window, &event_loop);
+
+    event_loop.run(move |event, _window_target, control_flow| {
+        let mut redraw = || {
+            let repaint_after = egui_glium.run(&window, |egui_ctx| {});
+
+            *control_flow = if repaint_after.is_zero() {
+                window.request_redraw();
+                event_loop::ControlFlow::Poll
+            } else if let Some(repaint_after_instant) =
+                std::time::Instant::now().checked_add(repaint_after)
+            {
+                event_loop::ControlFlow::WaitUntil(repaint_after_instant)
+            } else {
+                event_loop::ControlFlow::Wait
+            };
+
+            let mut target = display.draw();
+
+            target.clear_color_and_depth((0.0, 0.0, 0.0, 1.0), 1.0);
+            
+            egui_glium.paint(&display, &mut target);
+
+            target.finish().unwrap();
+        };
+        match event {
+            event::Event::RedrawRequested(_) => redraw(),
+
+            event::Event::WindowEvent { event, .. } => {
+                use event::WindowEvent;
+                match &event {
+                    WindowEvent::CloseRequested | WindowEvent::Destroyed => {
+                        *control_flow = event_loop::ControlFlow::Exit;
+                    }
+                    WindowEvent::Resized(new_size) => {
+                        display.resize((*new_size).into());
+                        width = new_size.width;
+                        height = new_size.height;
+                    }
+                    _ => {}
+                }
+
+                let event_response = egui_glium.on_event(&event);
+
+                if event_response.repaint {
+                    window.request_redraw();
+                }
+            }
+            event::Event::NewEvents(event::StartCause::ResumeTimeReached { .. }) => {
+                window.request_redraw();
+            }
+            _ => (),
+        }
+    });
+}
