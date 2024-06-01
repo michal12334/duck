0987160f5d4a996de097a0f0fe838d13567e91ed
Michal Boguszewski <michal1842001@gmail.com>
2024-06-01 12:13:17 +0200

    Make duck swim along bspline

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,9 @@ use egui::Shape::Mesh;
 use glium::{Display, Surface};
 use glium::glutin::surface::WindowSurface;
 use image::io::Reader;
-use nalgebra::{Matrix4, Point3, Vector3, Vector4};
+use nalgebra::{clamp, Matrix4, Point2, Point3, Vector2, Vector3, Vector4};
 use rand::prelude::ThreadRng;
-use rand::{RngCore, thread_rng};
+use rand::{Rng, RngCore, thread_rng};
 use winit::{event, event_loop};
 use winit::event::{MouseButton, WindowEvent};
 use winit::event::ElementState::Pressed;
@@ -35,9 +35,15 @@ fn main() {
         .build(&event_loop);
 
     let mut egui_glium = egui_glium::EguiGlium::new(&display, &window, &event_loop);
+
+    let mut rng = thread_rng();
     
     let duck_mesh = read_mesh("meshes/duck.txt", &display);
     let mesh_drawer = MeshDrawer::new(&display);
+    let mut duck_position = Point2::new(0.0, 0.0f32);
+    let mut b_spline = get_b_spline(duck_position, &mut rng);
+    let mut b_spline_t = 0.0f32;
+    let mut duck_speed = 0.01f32;
 
     let duck_texture = read_duck_texture(&display);
     let vulkan_texture = read_vulkan_texture(&display);
@@ -54,8 +60,6 @@ fn main() {
     let mut water_height = 0f32;
     let water_normal_computer = WaterNormalComputer::new(&display);
     let mut time_to_compute = 0.0f32;
-    
-    let mut rng = thread_rng();
 
     let mut mouse_position = (0.0, 0.0);
     let mut camera_direction = Vector3::new(0.0f32, 0.0, 1.0);
@@ -85,6 +89,18 @@ fn main() {
                         .step_by(0.05)
                         .text("water height")
                         .ui(ui);
+
+                    Slider::new(&mut duck_speed, -0.001..=0.1)
+                        .step_by(0.001)
+                        .text("Duck speed")
+                        .ui(ui);
+
+                    ui.label(format!("Duck position: ({:.1}, {:.1})", duck_position.x, duck_position.y));
+
+                    ui.label(format!("P[0]: ({:.1}, {:.1})", b_spline[0].x, b_spline[0].y));
+                    ui.label(format!("P[1]: ({:.1}, {:.1})", b_spline[1].x, b_spline[1].y));
+                    ui.label(format!("P[2]: ({:.1}, {:.1})", b_spline[2].x, b_spline[2].y));
+                    ui.label(format!("P[3]: ({:.1}, {:.1})", b_spline[3].x, b_spline[3].y));
                     
                     ui.label(format!("FPS: {:.1}", fps));
                 });
@@ -105,13 +121,22 @@ fn main() {
                 if x < 256 && y < 256 { 
                     water_normal_computer.bend(x as i32, y as i32);
                 }
+                
+                b_spline_t += duck_speed;
+                
+                if b_spline_t >= 1.0
+                {
+                    b_spline_t = 0.0;
+                    b_spline = [b_spline[1], b_spline[2], b_spline[3], get_random_point(b_spline[3], &mut rng)];
+                }
+                duck_position = get_b_spline_value(b_spline, b_spline_t);
 
                 water_normal_computer.compute();
                 
                 time_to_compute -= water_normal_computer.get_dt();
             }
             
-            mesh_drawer.draw(&mut target, &duck_mesh, &perspective, &view, &(Matrix4::new_translation(&Vector3::new(0.0, -0.2 + water_height * 5.0, 0.0)) * Matrix4::new_scaling(0.01)), &duck_texture);
+            mesh_drawer.draw(&mut target, &duck_mesh, &perspective, &view, &(Matrix4::new_translation(&Vector3::new(duck_position.x, -0.2 + water_height * 5.0, duck_position.y)) * Matrix4::new_scaling(0.01)), &duck_texture);
             cube_drawer.draw(&mut target, &cube, &perspective, &view, &Matrix4::new_scaling(5.0), &vulkan_texture, &sky_texture, &sand_texture);
             water_drawer.draw(&mut target, &water, &perspective, &view, &Matrix4::new_scaling(5.0), &Point3::from_slice((-camera_distant * camera_direction).as_slice()), water_height, &vulkan_texture, &sky_texture, &sand_texture, &water_normal_computer.normal_tex);
 
@@ -213,4 +238,28 @@ fn read_sand_texture(display: &Display<WindowSurface>) -> glium::texture::Textur
     let image_dimensions = image.dimensions();
     let image = glium::texture::RawImage2d::from_raw_rgba(image.into_raw(), image_dimensions);
     glium::texture::Texture2d::new(display, image).unwrap()
+}
+
+fn get_b_spline(p0: Point2<f32>, rng: &mut ThreadRng) -> [Point2<f32>; 4] {
+    let p1 = get_random_point(p0, rng);
+    let p2 = get_random_point(p1, rng);
+    let p3 = get_random_point(p2, rng);
+    [p0, p1, p2, p3]
+}
+
+fn get_random_point(p0: Point2<f32>, rng: &mut ThreadRng) -> Point2<f32> {
+    let direction = Vector2::new(rng.gen_range(-100..100) as f32, rng.gen_range(-100..100) as f32).normalize();
+    Point2::new(clamp(p0.x + direction.x, -5.0, 5.0), clamp(p0.y + direction.y, -5.0, 5.0))
+}
+
+fn get_b_spline_value(b_spline: [Point2<f32>; 4], t: f32) -> Point2<f32> {
+    let t2 = t * t;
+    let t3 = t2 * t;
+    let b0 = (-t3 + 3.0 * t2 - 3.0 * t + 1.0) / 6.0;
+    let b1 = (3.0 * t3 - 6.0 * t2 + 4.0) / 6.0;
+    let b2 = (-3.0 * t3 + 3.0 * t2 + 3.0 * t + 1.0) / 6.0;
+    let b3 = t3 / 6.0;
+    Point2::new(
+        b_spline[0].x * b0 + b_spline[1].x * b1 + b_spline[2].x * b2 + b_spline[3].x * b3,
+        b_spline[0].y * b0 + b_spline[1].y * b1 + b_spline[2].y * b2 + b_spline[3].y * b3)
 }
